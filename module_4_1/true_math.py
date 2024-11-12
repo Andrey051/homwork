4803879827ed29b62d4057f09c253763a255c857
Andrey <137562097+Andrey051@users.noreply.github.com>
2024-11-12 16:09:45 +0300

    Add files via upload

---
+++ module_4_1/true_math.py
@@ -0,0 +1,13 @@
+from math import inf
+def divide(first, second):
+    if second != 0:
+        division = first / second
+        return division
+    else:
+        return inf
+
+
+def main():
+    a = 10
+    b = 20
+    print("Привет")
